ffe1f246fe505147ff1d1f996681eb3be8877b57
Florian Knodt <git@adlerweb.info>
2024-01-30 18:09:53 +0100

    cleanup
    (aka: Make pylint happy)

--- lebra.py
+++ lebra.py
@@ -1,24 +1,28 @@
-import os
-import sys
+#!/usr/bin/env python3
+
+"""LeBRA - Legacy Barcode Reader Application"""
+
 import html
 import argparse
 import urllib.parse
+from datetime import datetime
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from pynput.keyboard import Key, Controller
-from datetime import datetime
 
 def emulate_key_presses(text):
+    """Send supplied text as key presses"""
     try:
         keyboard.type(text)
         if args.enter:
             keyboard.press(Key.enter)
             keyboard.release(Key.enter)
-    except Exception as e:
-        print(f"Error: {e}")
+    except Exception as error:
+        print(f"Error: {error}")
 
-class  lebraRequestHandler(BaseHTTPRequestHandler):
-           
+class LebraRequestHandler(BaseHTTPRequestHandler):
+    """HTTP handlers"""
     def do_GET(self):
+        """Handle GET requests and show form"""
         if self.path == '/':
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
@@ -37,31 +41,34 @@ class  lebraRequestHandler(BaseHTTPRequestHandler):
             self.end_headers()
 
     def do_POST(self):
+        """Handle POST requests and process barcodes"""
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length).decode('utf-8')
         parsed_data = urllib.parse.parse_qs(post_data)
-        
+
         if 'barcode' in parsed_data:
             barcode = parsed_data['barcode'][0]
-            barcodeHTML = html.escape(barcode)
+            barcode_html = html.escape(barcode)
             curdt = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             print(f"Scanned: {barcode} at {curdt}")
-            
+
             emulate_key_presses(barcode)
 
-            message = f'<div style="background-color:#060; color: white;">Success - Scanned {barcodeHTML} at {curdt}</div>'
+            message = '<div style="background-color:#060; color: white;">'
+            message = message + f'Success - Scanned {barcode_html} at {curdt}</div>'
         else:
             message = '<div style="background-color:#600; color: white;">ERROR</div>'
-            
+
         html_content = self.get_HTML(message)
-        
+
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
         self.wfile.write(html_content.encode('utf-8'))
-        
+
     def get_HTML(self, message=""):
-        html_content = """
+        """Generate HTML form"""
+        html_content = f"""
             <html>
                 <head>
                     <title>LeBRA - Legacy Bardocde Reader Application</title>
@@ -75,26 +82,29 @@ class  lebraRequestHandler(BaseHTTPRequestHandler):
                         <br>
                         <input type="submit" value="Submit">
                     </form>
-                    <div id="message">{}</div>
+                    <div id="message">{message}</div>
                 </body>
             </html>
-            """.format(message)
+            """
         return html_content
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Emulate keyboard input')
-    parser.add_argument('-e', '--enter', action='store_true', help='Press and release the Enter key after typing')
-    parser.add_argument('-l', '--listen', default='', metavar="IP-ADDRESS", help='Specify the listen address. Listen on all addresses if not specified.')
+    parser.add_argument('-e', '--enter', action='store_true',
+        help='Press and release the Enter key after typing')
+    parser.add_argument('-l', '--listen', default='', metavar="IP-ADDRESS",
+        help='Specify the listen address. Listen on all addresses if not specified.')
 
-    parser.add_argument('-p', '--port', type=int, default=8998, help='Specify the port number for HTTP server (default: 8998)')
+    parser.add_argument('-p', '--port', type=int, default=8998,
+        help='Specify the port number for HTTP server (default: 8998)')
 
 
     args = parser.parse_args()
-    
+
     server_address = (args.listen, args.port)
-    
+
     keyboard = Controller()
 
-    with HTTPServer(server_address,  lebraRequestHandler) as httpd:
+    with HTTPServer(server_address, LebraRequestHandler) as httpd:
         print(f'Starting server on {args.listen}:{args.port}...')
         httpd.serve_forever()
